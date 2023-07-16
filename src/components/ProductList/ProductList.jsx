92422afedeba2a6cb2b772651ee1ca666c980042
Vladimir Fedorov <nightsense@mail.ru>
2023-07-16 15:15:25 +0300

    jjj

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -43,7 +43,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('https://92.53.88.98:8000/web-data', {
+        fetch('http://92.53.88.98:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
